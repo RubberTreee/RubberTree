35e9e7d101e958a44a55e9f7623b046fe0c1c1d8
Loga Jegede <loga.jegede@gmail.com>
2018-09-27 20:39:54 +0100

    Use Postgres over sqlite3
    Heroku doesn't support sqlite3.
    More info here: https://devcenter.heroku.com/articles/sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_09_10_132401) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "prospects", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
